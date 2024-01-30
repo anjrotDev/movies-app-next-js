8bc83651de4c2eb60270795335a24121715758bc
Antonio Rodriguez <arodriguez-oc@ryanrts.com>
2024-01-30 17:41:29 -0300

    Link to home

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,21 @@
 import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import Switcher from "./Switcher";
 import Search from "./Search";
+import { AiTwotoneThunderbolt } from "react-icons/ai";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Navbar isBordered maxWidth="2xl">
       <NavbarContent justify="start">
-        <NavbarBrand className="mr-4">
-          <p className="hidden sm:block font-bold text-inherit">Anjrot Dev</p>
-        </NavbarBrand>
+        <Link href="/">
+          <NavbarBrand className="mr-4">
+            <span className="text-2xl">
+              <AiTwotoneThunderbolt />
+            </span>
+            <p className="hidden sm:block font-bold text-inherit">Anjrot Dev</p>
+          </NavbarBrand>
+        </Link>
       </NavbarContent>
       <NavbarContent as="div" className="items-center" justify="center">
         <Search />
